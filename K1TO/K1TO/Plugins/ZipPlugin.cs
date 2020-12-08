03b76e3434950754f7881bfbd546ad003be131a9
Caleb Mabry <36182383+caleb-mabry@users.noreply.github.com>
2020-12-07 22:55:27 -0500

    Update ZipPlugin.cs
    Actually used the variables I made....

--- K1TO/K1TO/Plugins/ZipPlugin.cs
+++ K1TO/K1TO/Plugins/ZipPlugin.cs
@@ -13,6 +13,7 @@ namespace K1TO.Plugins
         public int FileByteIdentifier => 0;
         public FileInformation FileInfo;
         public byte[] NewFileMagic = new byte[] { 0x50 };
+        public byte[] compressedData = new byte[] { };
         public int fileHeader;
         public short versionExtract;
         public short generalPurposeBitFlag;
@@ -31,69 +32,32 @@ namespace K1TO.Plugins
         public ZipPlugin(BinaryReader reader)
         {
             FileInfo = new FileInformation();
-            
-                using (reader)
+
+            using (reader)
+            {
+                Debug.WriteLine("The length of the file: " + reader.BaseStream.Length);
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
                 {
-                Debug.WriteLine("The length of the file: " +reader.BaseStream.Length);
                     fileHeader = reader.ReadInt32();
-                    while (fileHeader != 0x02014b50)
-                    {
-                        versionExtract = reader.ReadInt16();
-                        generalPurposeBitFlag = reader.ReadInt16();
-                        compressionMethod = reader.ReadInt16();
-                        fileLastModificationTime = reader.ReadInt16();
-                        fileLastModificationDate = reader.ReadInt16();
-                        crcUncompressed = reader.ReadInt32();
-                        compressedSize = reader.ReadInt32();
-                        uncompressedSize = reader.ReadInt32();
-                        fileNameLength = reader.ReadInt16();
-                        extraFieldLength = reader.ReadInt16();
-                        firstFileName = reader.ReadBytes(fileNameLength);
-                        extraInformation = reader.ReadBytes(extraFieldLength);
-                        // Add Filename
-                        if (Encoding.Default.GetString(firstFileName) != "")
-                        {
-                            var childFile = new FileInformation();
-                            childFile.filename = Encoding.Default.GetString(firstFileName);
-                            FileInfo.children.Add(childFile);
+                    versionExtract = reader.ReadInt16();
+                    generalPurposeBitFlag = reader.ReadInt16();
+                    compressionMethod = reader.ReadInt16();
+                    fileLastModificationTime = reader.ReadInt16();
+                    fileLastModificationDate = reader.ReadInt16();
+                    crcUncompressed = reader.ReadInt32();
+                    compressedSize = reader.ReadInt32();
+                    uncompressedSize = reader.ReadInt32();
+                    fileNameLength = reader.ReadInt16();
+                    extraFieldLength = reader.ReadInt16();
+                    firstFileName = reader.ReadBytes(fileNameLength);
+                    extraInformation = reader.ReadBytes(extraFieldLength);
+                    compressedData = reader.ReadBytes(compressedSize);
 
-                            // I need to see about removing this line. I think it's out of scope
-                            filesWithin.Add(Encoding.Default.GetString(firstFileName));
-
-                        }
-                        List<int> compressedData = new List<int>();
-                        var prevPosition = reader.BaseStream.Position;
-                        var potentialData = reader.ReadByte();
-                        var foundNewFile = false;
-                        while(foundNewFile == false)
-                        {
-                        Debug.WriteLine(reader.BaseStream.Position + " " + potentialData.ToString("X"));
-
-                        if (potentialData == NewFileMagic[0])
-                            {
-                                reader.BaseStream.Position = prevPosition;
-                                fileHeader = reader.ReadInt32();
-                                if (fileHeader == 0x4034B50 || fileHeader == 0x02014b50)
-                                {
-                                    foundNewFile = true;
-                                } 
-                                else
-                                {
-                                    compressedData.Add(potentialData);
-                                    potentialData = reader.ReadByte();  
-                                }
-                            } 
-                            else
-                            {
-
-                                compressedData.Add(potentialData);
-                                prevPosition = reader.BaseStream.Position;
-                                potentialData = reader.ReadByte();
-                            }
-                        }
-                    }
-
+                    var childFile = new FileInformation();
+                    childFile.filename = Encoding.Default.GetString(firstFileName);
+                    FileInfo.children.Add(childFile);
                 }
+            }
         }
     }
 }
